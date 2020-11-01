37332ce1019a41a5c0710f9994d2ded99ac4e859
Jorge Gabriel Azevedo <1160929@isep.ipp.pt>
2020-11-01 13:55:05 +0000

    move all loads and routes to loaders/

--- RedeMasterData/src/app.js
+++ RedeMasterData/src/app.js
@@ -1,48 +1,19 @@
-//Config env variables
-const dotenv = require('dotenv')
-dotenv.config();
-
+const loaders = require('./loaders');
 const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const cors = require('cors');
 
-// Imports routes
-const node = require('./routes/node.route');
+async function startServer() {
 
-const triptype = require('./routes/tripulantType.route')
+  const app = express();
 
-const line = require('./routes/line.route');
+  await loaders(app);
 
-const app = express();
-
-var corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+  app.listen(process.env.APP_PORT, err => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(`Server is up and running on port ${process.env.APP_PORT}`);
+  });
 }
 
-app.use(cors(corsOptions))
-
-//Connect with Database
-mongoose.connect(
-  process.env.DB_CONNECT,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  },
-  () => console.log('Connected with database')
-);
-
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-app.use('/tripulant-type', triptype);
-app.use('/node', node);
-app.use('/line', line);
-
-
-const port = process.env.APP_PORT || 3000;
-app.listen(port, () => {
-    console.log('Server is up and running on port ' + port);
-});
+startServer();
