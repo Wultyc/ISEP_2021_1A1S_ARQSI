70e35af959a0fafa8f2ac42b74cc8d2fff520fb9
Daniel Coelho <1160943@isep.ipp.pt>
2020-11-07 14:25:16 +0000

    Adjust line and route validations

--- RedeMasterData/src/models/route.model.js
+++ RedeMasterData/src/models/route.model.js
@@ -4,16 +4,32 @@ const Schema = mongoose.Schema;
 var Route = new Schema({
     distance: {
         type: Number,
-        required: [true, 'Insert a distance in kilometers.']
+        // determined by summing up the distance of its segments
     },
     duration: {
+        type: Number
+        // determined by summing up the durantion of its segments
+    },
+    startTime: {
         type: Number,
-        required: [true, 'Insert a duration in minutes.']
+        required: [true, 'Insert the routes start time in minutes.']
+    },
+    endTime: {
+        type: Number
+        // determined with startTime + duration
     },
     orientation: {
         type: String,
         enum: ['GoingRoute', 'ComingRoute'],
         required: [true, 'Insert a orientation.'],
+    },
+    isReinforcementRoute: {
+        type: Boolean,
+        required: [true, 'Insert if it is a reinforcement route.'],
+    },
+    isEmptyRoute: {
+        type: Boolean,
+        required: [true, 'Insert if it is an empty route.'],
     },
 
     // Lista de segmentos
@@ -22,8 +38,6 @@ var Route = new Schema({
         required: [true, 'Insert at least one segment.'],
     }]
 });
-
-// Route.index({ orientation: 1, segment: 1 }, { unique: true });
 
 // Export the model
 module.exports = mongoose.model('Route', Route);
