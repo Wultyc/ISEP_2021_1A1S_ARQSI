9c7a70b69a397cdfd7993d9e6de17cf7791607d2
Jorge Gabriel Azevedo <1160929@isep.ipp.pt>
2020-11-08 00:19:12 +0000

    update queryFilter to return correct sort object

--- RedeMasterData/src/libs/queryfilter.js
+++ RedeMasterData/src/libs/queryfilter.js
@@ -1,17 +1,19 @@
 const _ = require('lodash');
 
 
-module.exports.queryCleaner = function(query){
-    return _.omit(query,'sort','sortmode')
+module.exports.queryCleaner = function (query) {
+    const cleanQuery = _.omit(query, 'sort', 'sortmode')
+
+    return cleanQuery
 }
 
-module.exports.sortString = function(query){
+module.exports.sortString = function (query) {
     const field2Sort = query.sort || ""
     let sortMode
-    if(query.sortmode){
+    if (query.sortmode) {
         sortMode = (query.sortmode.toUpperCase() == 'DESC') ? -1 : 1
     } else {
         sortMode = ""
     }
-    return `{${field2Sort}:${sortMode}}`
+    return {[field2Sort]:sortMode}
 }
