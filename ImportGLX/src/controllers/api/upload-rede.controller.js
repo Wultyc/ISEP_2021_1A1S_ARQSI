ffd4cd01b751a6a8256b2bf63d6987e035691cac
Jorge Gabriel Azevedo <1160929@isep.ipp.pt>
2020-11-14 16:39:03 +0000

    Parsing XML to JSON

--- ImportGLX/src/controllers/api/upload-rede.controller.js
+++ ImportGLX/src/controllers/api/upload-rede.controller.js
@@ -1,5 +1,26 @@
 const config = require('config')
+const xml2js = require('xml2js')
+const fs = require('fs')
 
 exports.start = function (fileName) {
-    console.log("Upload Rede not implemented")
+    const filepath = `${__dirname}/../../${config.get('glx-files.upload-dir')}/${fileName}`
+    const parser = new xml2js.Parser();
+
+    fs.readFile( filepath, function (err, data) {
+        parser.parseString(data, function (err, result) {
+
+            if(err){
+                console.log(err)
+                return err
+            }
+
+            console.log('File Parsed successfully');
+            processGLX(result)
+        });
+    });
+}
+
+function processGLX(data) {
+    const network = data.GlDocumentInfo.world[0].GlDocument[0].GlDocumentNetwork[0].Network[0]
+    console.log(network)
 }
