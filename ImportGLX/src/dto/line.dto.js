84a45de2110c083dd886ff3e8309a447ccda93f3
Jorge Gabriel Azevedo <1160929@isep.ipp.pt>
2020-11-15 16:49:20 +0000

    Include begin node, final node and line color

--- ImportGLX/src/dto/line.dto.js
+++ ImportGLX/src/dto/line.dto.js
@@ -1,12 +1,14 @@
 LineDTO = function (glx_entry, routeList) {
+    const exampleRoute = routeList.find((route) => route.glx_id == glx_entry.LinePaths[0].LinePath[0].$.Path)
     let line = {
         system_id: "",
         glx_id: glx_entry.$.key,
         data: {
             code: "",
             name: glx_entry.$.Name,
-            beginNode: "",
-            finalNode: "",
+            color: glx_entry.$.Color,
+            beginNode: exampleRoute.data.routeNodes[0].nodeId,
+            finalNode: exampleRoute.data.routeNodes[exampleRoute.data.routeNodes.length - 1].nodeId,
             route: glx_entry.LinePaths[0].LinePath.map(v => ({routeId:routeList.find((route) => route.glx_id == v.$.Path).system_id, orientation:v.$.Orientation})),
             tripulantType: "",
             vehicleType: ""
