5f673cf21123ec7afb982325f689cc4c863feed5
Jorge Gabriel Azevedo <1160929@isep.ipp.pt>
2020-11-15 19:18:30 +0000

    convert bool to lowercase to allow cast on end system

--- ImportGLX/src/dto/route.dto.js
+++ ImportGLX/src/dto/route.dto.js
@@ -4,7 +4,7 @@ RouteDTO = function (glx_entry, nodeList) {
         glx_id: glx_entry.$.key,
         data: {
             isReinforcementRoute: false,
-            isEmptyRoute: glx_entry.$.IsEmpty,
+            isEmptyRoute: glx_entry.$.IsEmpty.toLowerCase(),
             routeNodes: glx_entry.PathNodes[0].PathNode.map(v => ({nodeId: nodeList.find((node) => node.glx_id == v.$.Node).system_id, distance: v.$.Distance || "0", duration: v.$.Duration || "0"}))
         },
         status: ""
