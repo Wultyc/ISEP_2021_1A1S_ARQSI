42c3b23647c9e51e6cb0df7162575eef6d69faeb
Jorge Gabriel Azevedo <1160929@isep.ipp.pt>
2020-11-15 16:49:33 +0000

    remove unsed props

--- ImportGLX/src/dto/route.dto.js
+++ ImportGLX/src/dto/route.dto.js
@@ -3,9 +3,6 @@ RouteDTO = function (glx_entry, nodeList) {
         system_id: "",
         glx_id: glx_entry.$.key,
         data: {
-            //distance: "",
-            //duration: "",
-            orientation: "",
             isReinforcementRoute: "False",
             isEmptyRoute: glx_entry.$.IsEmpty,
             routeNodes: glx_entry.PathNodes[0].PathNode.map(v => ({nodeId: nodeList.find((node) => node.glx_id == v.$.Node).system_id, distance: v.$.Distance || "0", duration: v.$.Duration || "0"}))
