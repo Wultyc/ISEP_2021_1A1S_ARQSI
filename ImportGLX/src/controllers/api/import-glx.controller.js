ddd81daf61e8773c15c799270d28a883134688c5
Jorge Gabriel Azevedo <1160929@isep.ipp.pt>
2020-11-15 18:59:55 +0000

    fix typo on success message

--- ImportGLX/src/controllers/api/import-glx.controller.js
+++ ImportGLX/src/controllers/api/import-glx.controller.js
@@ -28,6 +28,6 @@ exports.importfile = async function (req, res) {
         return res.status(config.get('errors.import.error-on-upload.status')).json(config.get('errors.import.error-on-upload.message'))
     }
 
-    res.send("Imported started successfully")
+    res.send("Import started successfully")
 
 }
