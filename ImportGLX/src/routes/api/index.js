064d40cbb783ddaf81fb40f6dbbda97930f96388
Jorge Gabriel Azevedo <1160929@isep.ipp.pt>
2020-12-03 19:39:02 +0000

    add healthcheck endpoint

--- ImportGLX/src/routes/api/index.js
+++ ImportGLX/src/routes/api/index.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const importGlx = require('./import-glx')
 
 router.use('/import-glx', importGlx)
+router.get('/healthcheck', (req, res) => {res.send("I'm Alive")})
 
 module.exports = router
