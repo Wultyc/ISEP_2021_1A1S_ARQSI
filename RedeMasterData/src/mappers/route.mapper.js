28ba3200575203cd2b43efca98cbf04992113a3f
Jorge Gabriel Azevedo <1160929@isep.ipp.pt>
2020-12-05 21:38:07 +0000

    map null distance and duration as 0 in route mapper

--- RedeMasterData/src/mappers/route.mapper.js
+++ RedeMasterData/src/mappers/route.mapper.js
@@ -2,8 +2,8 @@ class RouteMapper {
     constructor(){}
     fromReqToDTO = function (reqBody, dto) {
             dto.id = reqBody.id
-            dto.distance = reqBody.distance
-            dto.duration = reqBody.duration
+            dto.distance = reqBody.distance || 0
+            dto.duration = reqBody.duration || 0
             dto.isReinforcementRoute = reqBody.isReinforcementRoute
             dto.isEmptyRoute = reqBody.isEmptyRoute
             dto.routeNodes = reqBody.routeNodes
