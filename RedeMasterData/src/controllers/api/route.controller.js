8cdc9af19957b82c6fc8f524c933c72272893a97
Jorge Gabriel Azevedo <1160929@isep.ipp.pt>
2020-12-05 16:03:51 +0000

    validate request before sending to service

--- RedeMasterData/src/controllers/api/route.controller.js
+++ RedeMasterData/src/controllers/api/route.controller.js
@@ -1,15 +1,15 @@
 const _ = require('lodash');
 const routeDTO = require('../../dto/route.dto');
 const RoutesMapper = require('../../mappers/route.mapper');
-const Route = require('../../models/route.model');
 const ServiceRoute = require('../../services/route.service');
+const routesValidationSchema = require('../../validations/routes.validation');
 
 const service = new ServiceRoute();
 const routeMapper = new RoutesMapper();
 
 exports.routeGetById = function (req, res) {
-    service.routeGetById(req.params.routeId, function(err, params) {
-        if(err){
+    service.routeGetById(req.params.routeId, function (err, params) {
+        if (err) {
             return res.status(404).send(err);
         }
         const response = routeMapper.fromReqToDTO(params, new routeDTO);
@@ -18,8 +18,8 @@ exports.routeGetById = function (req, res) {
 };
 
 exports.routeGetAll = function (req, res) {
-    service.routeGetAll(function(err, params) {
-        if(err){
+    service.routeGetAll(function (err, params) {
+        if (err) {
             return res.status(404).send(err);
         }
         res.status(200).json(params)
@@ -28,6 +28,10 @@ exports.routeGetAll = function (req, res) {
 
 exports.routeCreate = function (req, res) {
     let route = routeMapper.fromReqToDTO(req.body, new routeDTO);
+
+    const { error } = routesValidationSchema.validate(route);
+    if (error) return res.status(400).send({ message: "some fields are missing or have invalid values", error: error })
+
     service.routeCreate(route, function (err, params) {
         if (err) {
             return res.status(400).send(err);
