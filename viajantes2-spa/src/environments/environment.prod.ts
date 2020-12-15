87322ee0785c4e7bd49fe476fe435f39ed00f77e
Jorge Gabriel Azevedo <1160929@isep.ipp.pt>
2020-12-15 19:59:06 +0000

    update backend services URL

--- viajantes2-spa/src/environments/environment.prod.ts
+++ viajantes2-spa/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiEndpointBackendRDM: 'http://redemasterdata/api/',
-  apiEndpointBackendGLX: 'http://importglx/api/import-glx'
+  apiEndpointBackendRDM: 'http://redemasterdata.viajantes2.tk/api/',
+  apiEndpointBackendGLX: 'http://importglx.viajantes2.tk/api/import-glx'
 };
