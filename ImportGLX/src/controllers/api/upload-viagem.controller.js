dbb242bbde85a56fc09672672ced8c3528b0eba6
Jorge Gabriel Azevedo <1160929@isep.ipp.pt>
2020-12-19 15:31:10 +0000

    Delere files from importglx

--- ImportGLX/src/controllers/api/upload-viagem.controller.js
+++
@@ -1,5 +0,0 @@
-const config = require('config')
-
-exports.start = async function (fileName) {
-    console.log("Upload Viagem not implemented")
-}
--- ImportGLX/src/routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const importGlx = require('./import-glx')
-
-router.use('/import-glx', importGlx)
-router.get('/healthcheck', (req, res) => {res.send("I'm Alive")})
-
-module.exports = router
