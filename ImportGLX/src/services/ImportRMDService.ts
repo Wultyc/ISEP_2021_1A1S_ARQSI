836bef6540b57518fcc65e9b20fc15b85d2d97fb
Jorge Gabriel Azevedo <1160929@isep.ipp.pt>
2020-12-22 16:12:32 +0000

    Implement logger on ImportRMDService and MasterDataTRepository

--- ImportGLX/src/services/ImportRMDService.ts
+++ ImportGLX/src/services/ImportRMDService.ts
@@ -15,6 +15,7 @@ import RouteMapper from '../mappers/RouteMapper'
 import LineMapper from '../mappers/LineMapper'
 
 import { config } from 'node-config-ts'
+import logger from '../utils/Logger'
 
 export default class StoreGLXService implements IService {
 
@@ -49,24 +50,28 @@ export default class StoreGLXService implements IService {
     }
 
     private async processVehicleType() {
+        new logger().log("Processing Vehicle Types")
         const vtmap = new VehicleTypeMapper()
         this.vehicleTypes = this.network.VehicleTypes[0].VehicleType.map(v => vtmap.mapFromGLX(v.$, new vehicleTypeDTO))
         await this.callRepository(config.endpoints.redeMasterData.vehicleType, this.vehicleTypes, new MasterDataRepository())
     }
 
     private async processNodes() {
+        new logger().log("Processing Nodes")
         const nmap = new NodeMapper()
         this.nodes = this.network.Nodes[0].Node.map(v => nmap.mapFromGLX(v.$, new NodeDto))
         await this.callRepository(config.endpoints.redeMasterData.node, this.nodes, new MasterDataRepository())
     }
 
     private async processRoutes() {
+        new logger().log("Processing Routes")
         const rmap = new RouteMapper()
         this.routes = this.network.Paths[0].Path.map(v => rmap.mapFromGLX(v, new RouteDto, this.nodes))
         await this.callRepository(config.endpoints.redeMasterData.route, this.routes, new MasterDataRepository())
     }
 
     private async processLines() {
+        new logger().log("Processing Lines")
         const lmap = new LineMapper()
         this.lines = this.network.Lines[0].Line.map(v => lmap.mapFromGLX(v, new LineDto, this.routes))
         await this.callRepository(config.endpoints.redeMasterData.line, this.lines, new MasterDataRepository())
