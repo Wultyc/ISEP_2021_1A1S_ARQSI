c67776b8402dd9c4355e6bb8e6c67d2263651614
Jorge Gabriel Azevedo <1160929@isep.ipp.pt>
2020-12-23 21:49:34 +0000

    Joi schemas for validation

---
+++ RedeMasterData/src/models/joi/Lines.ts
@@ -0,0 +1,18 @@
+const Joi = require('joi');
+
+const linesValidationSchema = new Joi.object({
+    id: Joi.string(),
+    code: Joi.string().required(),
+    name: Joi.string().required(),
+    color: Joi.string().required(),
+    beginNode: Joi.string(),
+    finalNode: Joi.string(),
+    lineRoutes: Joi.array().items({
+        routeId: Joi.string().required(),
+        orientation: Joi.string().required()
+    }).required(),
+    tripulantType: Joi.array().items(Joi.string()).required(),
+    vehicleType: Joi.array().items(Joi.string()).required()
+})
+
+module.exports = linesValidationSchema;
