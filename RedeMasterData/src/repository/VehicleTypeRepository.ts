bfb71a4a97391c9387a3e254c6191bbe7ca7103b
Jorge Gabriel Azevedo <1160929@isep.ipp.pt>
2020-12-23 23:58:48 +0000

    update repositories defenitions

--- RedeMasterData/src/repository/VehicleTypeRepository.ts
+++ RedeMasterData/src/repository/VehicleTypeRepository.ts
@@ -1,13 +1,14 @@
 import IRepository from './interface/IRepository'
+import Result from '../utils/Result';
 
 export default class VehicleTypeRepository implements IRepository{
     save(): boolean | Promise<boolean> {
         throw new Error('Method not implemented.');
     }
-    load(query: any, sort: any) {
+    load(query: any, sort: any):Result {
         throw new Error('Method not implemented.');
     }
-    loadById(id: any) {
+    loadById(id: any):Result {
         throw new Error('Method not implemented.');
     }
 
