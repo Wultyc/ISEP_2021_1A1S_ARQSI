fdcc4bce2f7648bb84f19722105f28ef9b05cf59
Jorge Gabriel Azevedo <1160929@isep.ipp.pt>
2020-12-25 23:39:00 +0000

    update definition

--- RedeMasterData/src/repository/TripulantTypeRepository.ts
+++ RedeMasterData/src/repository/TripulantTypeRepository.ts
@@ -5,16 +5,16 @@ import { Result } from '../core/logic/Result'
 import IDto from '../dto/interface/IDto'
 
 export default class TripulantTypeRepository implements IRepository{
-    save(dto: IDto, callback: any): Promise<Result<IDto>> {
+    save(dto: IDto): Promise<Result<IDto>> {
         throw new Error('Method not implemented.')
     }
-    load(query: any, sort: any, callback: any): Promise<Result<IDto>> {
+    load(query: any, sort: any): Promise<Result<IDto>> {
         throw new Error('Method not implemented.')
     }
-    loadById(id: string, callback: any): Promise<Result<IDto>> {
+    loadById(id: string): Promise<Result<IDto>> {
         throw new Error('Method not implemented.')
     }
-    delete(id: string, callback: any): Promise<Result<IDto>> {
+    delete(id: string): Promise<Result<IDto>> {
         throw new Error('Method not implemented.')
     }
 
--- RedeMasterData/src/repository/interface/IRepository.ts
+++ RedeMasterData/src/repository/interface/IRepository.ts
@@ -1,8 +1,8 @@
 import IDto from '../../dto/interface/IDto'
 import { Result } from '../../core/logic/Result';
 export default interface IRepository{
-    save(dto:IDto,callback): Promise<Result<IDto>>
-    load(query:any,sort:any,callback): Promise<Result<IDto>>
-    loadById(id:string,callback): Promise<Result<IDto>>
-    delete(id:string,callback): Promise<Result<IDto>>
+    save(dto:IDto): Promise<Result<IDto>>
+    load(query:any,sort:any): Promise<Result<IDto>>
+    loadById(id:string): Promise<Result<IDto>>
+    delete(id:string): Promise<Result<IDto>>
 }
