8a93cbea1277844011907ac8e0b80ceb65048fc2
Daniel Coelho <1160943@isep.ipp.pt>
2020-12-28 22:03:48 +0000

    Add Trip create DB tables script

--- ViagemMasterData/SQL/dbo.Schedule.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Schedule]
-(
- [Id] Varchar NOT NULL PRIMARY KEY,
- [Trip] Varchar(Max) NOT NULL,
- [StartTime] Time NOT NULL,
- [EndTime] Time NOT NULL
-)
-
--- Set Id as PK
--- ViagemMasterData/SQL/dbo.Trip.sql
+++ ViagemMasterData/SQL/dbo.Trip.sql
@@ -3,7 +3,8 @@
  [Id] Varchar NOT NULL PRIMARY KEY,
  [Line] Varchar(Max) NOT NULL,
  [Route] Varchar(Max) NOT NULL,
- [StartHour] Time NOT NULL
+ [StartTime] Time NOT NULL,
+ [EndTime] Time NOT NULL
 )
 
 -- Set Id as PK
