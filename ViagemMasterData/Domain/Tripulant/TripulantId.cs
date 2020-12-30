1a3773f5a2df0851ec161416fbc75aebc3887b8d
Nuno Dinis <1161042@isep.ipp.pt>
2020-12-30 16:40:36 +0000

    Boiler plate for tripulant entity.

---
+++ ViagemMasterData/Domain/Tripulant/TripulantId.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using ViagemMasterData.Domain.Shared;
+
+namespace ViagemMasterData.Domain.Tripulant
+{
+    public class TripulantId : EntityId, IValueObject
+
+    {
+        [JsonConstructor]
+        public TripulantId(Guid value) : base(value)
+        {
+        }
+
+        public TripulantId(String value) : base(value)
+        {
+        }
+
+        protected override object createFromString(string text)
+        {
+            return new Guid(text);
+        }
+
+        public override String AsString()
+        {
+            Guid obj = (Guid)base.ObjValue;
+            return obj.ToString();
+        }
+
+        public Guid AsGuid()
+        {
+            return (Guid)base.ObjValue;
+        }
+    }
+}
---
+++ ViagemMasterData/Domain/Tripulant/TripulantValidator.cs
@@ -0,0 +1,41 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ViagemMasterData.Domain.Tripulant
+{
+    public class TripulantValidator : AbstractValidator<Tripulant>
+    {
+        public TripulantValidator()
+        {
+            RuleFor(c => c)
+                .NotNull()
+                .OnAnyFailure(x =>
+                {
+                    throw new ArgumentNullException("Can't found the object.");
+                });
+
+            RuleFor(c => c.Id)
+                .NotEmpty().WithMessage("Is necessary to inform the code.")
+                .NotNull().WithMessage("Is necessary to inform the code.");
+
+            RuleFor(c => c.Name)
+                    .NotEmpty().WithMessage("Is necessary to inform the licence plate.")
+                    .NotNull().WithMessage("Is necessary to inform the licence plate.");
+
+            RuleFor(c => c.BirthDate)
+                    .NotEmpty().WithMessage("Is necessary to inform the vin.")
+                    .NotNull().WithMessage("Is necessary to inform the vin.");
+
+            RuleFor(c => c.LicenceNr)
+                    .NotEmpty().WithMessage("Is necessary to inform the vehicle type.")
+                    .NotNull().WithMessage("Is necessary to inform the vehicle type.");
+
+            RuleFor(c => c.LicenceExpires)
+                    .NotEmpty().WithMessage("Is necessary to inform the start date.")
+                    .NotNull().WithMessage("Is necessary to inform the start date.");
+        }
+    }
+}
