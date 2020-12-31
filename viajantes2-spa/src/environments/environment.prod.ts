493cb5c9c49848515c3f5739b928a19e530d7ca7
Nuno Dinis <1161042@isep.ipp.pt>
2020-12-31 17:31:28 +0000

    Boiler plate for trip SPA.

--- viajantes2-spa/src/environments/environment.prod.ts
+++ viajantes2-spa/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiEndpointBackendRDM: 'http://redemasterdata.viajantes2.tk/api/',
-  apiEndpointBackendGLX: 'http://importglx.viajantes2.tk/api/import-glx'
+  apiEndpointBackendGLX: 'http://importglx.viajantes2.tk/api/import-glx',
+  apiEndpointBackendVDM: 'http://viagemmasterdata.viajantes2.tk/api/'
 };
