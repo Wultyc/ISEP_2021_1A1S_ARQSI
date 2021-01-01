db7b45a8798dcf9b6d43bee6e1e1033563983140
Nuno Dinis <1161042@isep.ipp.pt>
2021-01-01 23:50:53 +0000

    Domain for Vehicle Service.

---
+++ ViagemMasterData/Domain/VehicleServices/VehicleServiceValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ViagemMasterData.Domain;
+
+namespace ViagemMasterData.Domain.VehicleServices
+{
+    public class VehicleServiceValidator : AbstractValidator<VehicleServices>
+    {
+        public VehicleServiceValidator()
+        {
+
+            RuleFor(c => c)
+                .NotNull()
+                .OnAnyFailure(x =>
+                {
+                    throw new ArgumentNullException("Can't found the object.");
+                });
+
+            RuleFor(c => c.VehicleId)
+                .NotEmpty().WithMessage("Is necessary to inform the Vehicle Id.")
+                .NotNull().WithMessage("Is necessary to inform the Vehicle Id.");
+
+            RuleFor(c => c.Date)
+                .NotEmpty().WithMessage("Is necessary to inform the date.")
+                .NotNull().WithMessage("Is necessary to inform the date.");
+        }
+    }
+}
---
+++ ViagemMasterData/Domain/VehicleServices/VehicleServices.cs
@@ -0,0 +1,49 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ViagemMasterData.Domain.Shared;
+using ViagemMasterData.Domain.Vehicles;
+using ViagemMasterData.Infrastructure;
+
+namespace ViagemMasterData.Domain.VehicleServices
+{
+    public class VehicleServices : Entity<VehicleServiceId>, IAggregateRoot
+    {
+        public VehicleId VehicleId { get; set; }
+        public DateTime Date { get; set; }
+
+        private VehicleServices()
+        {
+        }
+
+        public VehicleServices(string id, string vehicleId, DateTime date)
+        {
+            if (id != null)
+            {
+                this.Id = new VehicleServiceId(id);
+            }
+            else
+            {
+                this.Id = new VehicleServiceId(Guid.NewGuid().ToString().ToUpper());
+            }
+            this.VehicleId = new VehicleId(vehicleId);
+            this.Date = date;
+        }
+
+        public async Task Validate(VehicleServices vehicleServices)
+        {
+            var dbContext = new BaseContext();
+            VehicleServiceValidator validator = new VehicleServiceValidator();
+            validator.ValidateAndThrow(vehicleServices);
+
+            var vehicle = dbContext.Vehicles.Where(b => b.Id == vehicleServices.VehicleId.Value.ToString()).FirstOrDefault();
+            if (vehicle == null)
+            {
+                throw new BusinessRuleValidationException("Vehicle " + vehicleServices.VehicleId.Value.ToString() + " not found!");
+            }
+        }
+
+    }
+}
