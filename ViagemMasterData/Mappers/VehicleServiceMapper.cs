6d9d399c74652e29c9d4b183a48b1455059a74b7
Nuno Dinis <1161042@isep.ipp.pt>
2021-01-01 23:51:51 +0000

    VehicleService mapper added.

---
+++ ViagemMasterData/Mappers/VehicleServiceMapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ViagemMasterData.DTOs.VehicleServiceDTOs;
+using ViagemMasterData.Schema;
+
+namespace ViagemMasterData.Mappers
+{
+    public class VehicleServiceMapper
+    {
+        public VehicleServiceMapper() { }
+        public VehicleServiceDTO GetDTOFromSchema(Schema.VehicleService vehicleService)
+        {
+            return new VehicleServiceDTO(vehicleService.Id.ToString().ToUpper(),
+                vehicleService.VehicleId, vehicleService.Date, vehicleService.WorkBlocks.ToList());
+        }
+
+        public Domain.VehicleServices.VehicleServices GetDomainFromTripulantDTO(VehicleServiceDTO vehicleServiceDTO)
+        {
+            return new Domain.VehicleServices.VehicleServices(vehicleServiceDTO.Id, vehicleServiceDTO.VehicleId, vehicleServiceDTO.Date);
+        }
+
+        public Schema.VehicleService GetSchemaFromDomain(Domain.VehicleServices.VehicleServices vehicleServices)
+        {
+
+            return new Schema.VehicleService(vehicleServices.Id.Value.ToString(), vehicleServices.VehicleId.Value.ToString(), vehicleServices.Date);
+        }
+
+        public VehicleServiceDTO GetTripulantDTOFromCreateTripulantDTO(CreateVehicleServiceDTO createVehicleServiceDTO)
+        {
+            return new VehicleServiceDTO(null,
+                createVehicleServiceDTO.VehicleId, createVehicleServiceDTO.Date, null);
+        }
+    }
+}
