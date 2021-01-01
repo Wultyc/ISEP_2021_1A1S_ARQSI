640171db56cb5810a05c05f91aea04e9fc8c6ae1
Daniel Coelho <1160943@isep.ipp.pt>
2021-01-01 16:33:42 +0000

    List trip with line and route and without ids

--- viajantes2-spa/src/app/ViagemMasterData/models/trip.ts
+++ viajantes2-spa/src/app/ViagemMasterData/models/trip.ts
@@ -1,22 +1,24 @@
 import { Time } from "@angular/common";
+import { Line } from "src/app/RedeMasterData/models/line";
+import { Route } from "src/app/RedeMasterData/models/route";
 
 export class Trip {
     id: string;  
-    lineId: string;
-    routeId: string;
+    line: Line;
+    route: Route;
     startTime: Time;
     endTime: Time;
 }
 
 export class TripAdHocPost {
-    lineId: string;
-    routeId: string;
+    line: Line;
+    route: Route;
     startTime: Time;
 }
 
 export class TripPost {
-    lineId: string;
-    routeId: string;
+    line: Line;
+    route: Route;
     startTime: Time;
     frequency: number;
     numberOfTrips: number;
