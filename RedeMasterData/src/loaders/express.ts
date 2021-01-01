10b6f62f8f6b2ca9fef2168f6d9548b8974a0990
Jorge Gabriel Azevedo <1160929@isep.ipp.pt>
2021-01-01 04:03:03 +0000

    update cors config on RMD

--- RedeMasterData/src/loaders/express.ts
+++ RedeMasterData/src/loaders/express.ts
@@ -1,13 +1,26 @@
-import {Application} from 'express'
+import { Application } from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
-import {router} from '../routes'
+import { router } from '../routes'
 import { accessLogger } from '../middlewares'
+import {config} from 'node-config-ts'
 
 const expressLoader = function (app: Application): Application {
 
     //config express server
-    app.use(cors())
+    var allowedDomains = config.cors.allowedDomains;
+    app.use(cors({
+        origin: function (origin, callback) {
+            // bypass the requests with no origin (like curl requests, mobile apps, etc )
+            if (!origin) return callback(null, true);
+
+            if (allowedDomains.indexOf(origin) === -1) {
+                var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+                return callback(new Error(msg), false);
+            }
+            return callback(null, true);
+        }
+    }));
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -21,4 +34,4 @@ const expressLoader = function (app: Application): Application {
     return app;
 }
 
-export {expressLoader}
+export { expressLoader }
