8bfb8f64a50b0ff66ba9c04a9a04c69bc911f93a
Nuno Dinis <1161042@isep.ipp.pt>
2021-01-01 23:52:29 +0000

    Vehicle service service started.

---
+++ ViagemMasterData/Service/VehicleServiceService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ViagemMasterData.Domain.Shared;
+using ViagemMasterData.Infraestructure;
+using ViagemMasterData.Mappers;
+
+namespace ViagemMasterData.Service
+{
+    public class VehicleServiceService
+    {
+        private readonly VehicleServiceMapper vehicleServiceMapper = new VehicleServiceMapper();
+        
+        private readonly IRepository<Schema.VehicleService> _repository;
+    }
+}
