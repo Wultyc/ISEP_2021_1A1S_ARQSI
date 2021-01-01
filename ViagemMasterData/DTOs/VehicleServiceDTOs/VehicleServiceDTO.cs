92cc2c9f17b87a453037814dd10e30ea34a84e6b
Nuno Dinis <1161042@isep.ipp.pt>
2021-01-01 23:52:05 +0000

    Vehicle Service DTO implementation.

---
+++ ViagemMasterData/DTOs/VehicleServiceDTOs/VehicleServiceDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ViagemMasterData.Schema;
+
+namespace ViagemMasterData.DTOs.VehicleServiceDTOs
+{
+    public class VehicleServiceDTO
+    {
+        public string Id { get; set; }
+        public string VehicleId { get; set; }
+        public DateTime Date { get; set; }
+        public List<WorkBlock> WorkBlocks { get; set; }
+
+        public VehicleServiceDTO(string id, string vehicleId, DateTime date, List<WorkBlock> workBlocks)
+        {
+            this.Id = id;
+            this.VehicleId = vehicleId;
+            this.Date = date;
+            this.WorkBlocks = workBlocks;
+        }
+    }
+}
