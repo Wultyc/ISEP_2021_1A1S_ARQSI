fa1a4e191afdb6361376b4819bdbec7e8bcb977e
Nuno Dinis <1161042@isep.ipp.pt>
2021-01-19 18:39:29 +0000

    Vehicle type mapper units

--- RedeMasterData/src/tests/unit-tests/mappers-unit.ts
+++ RedeMasterData/src/tests/unit-tests/mappers-unit.ts
@@ -3,7 +3,10 @@ import { expect } from 'chai';
 import NodeMapper from "../../mappers/NodeMapper"
 import NodeDTO from '../../dto/NodeDTO';
 import Node from '../../domain/Nodes/Node'
-describe('mapFromRequest', () =>
+import VehicleTypeDTO from '../../dto/VehicleTypeDTO';
+import VehicleTypeMapper from '../../mappers/VehicleTypeMapper'
+import VehicleType from '../../domain/VehicleType/VehicleType'
+describe('Mapper for node', () =>
 {
   describe('test mapFromRequest(req: any, dto: NodeDTO)', () =>
   {
@@ -65,7 +68,7 @@ describe('mapFromRequest', () =>
   });
   describe('test mapFromMongo(req: any, dto: NodeDTO)', () =>
   {
-    it('should map correctly request to dto object from mongo', () =>
+    it('should map correctly domain to dto', () =>
     {
 
       let dto: NodeDTO = {
@@ -82,7 +85,103 @@ describe('mapFromRequest', () =>
       let mapper: NodeMapper = new NodeMapper();
       expect(mapper.mapFromDomain(domainNode.getValue(), new NodeDTO))
       .to
+      .include( {
+        shortName: dto.shortName,
+        name: dto.name,
+        longitude: dto.longitude,
+        latitude: dto.latitude,
+        collectionNode: dto.collectionNode,
+        surrenderNode: dto.surrenderNode
+      })
+    });
+  });
+});
+describe('Mapper for vehicle type', () =>
+{
+  describe('test mapFromRequest(req: any, dto: VehicleTypeDTO)', () =>
+  {
+    it('should map correctly request to dto object', () =>
+    { 
+      let vehicleTypeDTO: VehicleTypeDTO = {
+        id: 'TipoID',
+        description: 'Tipo de veiculo',
+        autonomy: 50,
+        costPerKilometer: 50,
+        averageCost: 50,
+        averageSpeed: 50,
+        fuelType: 'Azeite'
+      }
+      let body: any = {
+        body: {
+          id: 'TipoID',
+          description: 'Tipo de veiculo',
+          autonomy: 50,
+          costPerKilometer: 50,
+          averageCost: 50,
+          averageSpeed: 50,
+          fuelType: 'Azeite'
+        }
+    }
+    let mapper: VehicleTypeMapper = new VehicleTypeMapper()
+    expect(mapper.mapFromRequest(body, new VehicleTypeDTO))
+    .to
+    .eql(vehicleTypeDTO);
+    });
+  });
+  describe('test map(req: any, dto: VehicleTypeDTO)', () =>
+  {
+    it('should map correctly request to dto object from mongo', () =>
+    {
+        let mongo: any = {
+          _id: 'TipoID',
+          description: 'Tipo de veiculo',
+          autonomy: 50,
+          costPerKilometer: 50,
+          averageCost: 50,
+          averageSpeed: 50,
+          fuelType: 'Azeite'
+        }
+        let dto: VehicleTypeDTO = {
+          id: 'TipoID',
+          description: 'Tipo de veiculo',
+          autonomy: 50,
+          costPerKilometer: 50,
+          averageCost: 50,
+          averageSpeed: 50,
+          fuelType: 'Azeite'
+        }
+      let mapper: VehicleTypeMapper = new VehicleTypeMapper();
+      expect(mapper.mapFromMongo(mongo, new VehicleTypeDTO))
+      .to
       .eql(dto);
+    });
+  });
+  describe('test mapFromDomain(req: any, dto: VehicleTypeDTO)', () =>
+  {
+    it('should map correctly request to dto object from mongo', () =>
+    {
+        let dto: VehicleTypeDTO = {
+          id: 'TipoID',
+          description: 'Tipo de veiculo',
+          autonomy: 50,
+          costPerKilometer: 50,
+          averageCost: 50,
+          averageSpeed: 50,
+          fuelType: 'Gasoleo'
+        }
+      let mapper: VehicleTypeMapper = new VehicleTypeMapper();
+      const newDomainVehicleType = VehicleType.create(dto)
+
+      expect(mapper.mapFromDomain(newDomainVehicleType.getValue(), new VehicleTypeDTO))
+      .to
+      .include( {
+        description: dto.description,
+        autonomy: dto.autonomy,
+        costPerKilometer: dto.costPerKilometer,
+        averageCost: dto.averageCost,
+        averageSpeed: dto.averageSpeed,
+        fuelType: dto.fuelType
+      })
     });
   });
 });
