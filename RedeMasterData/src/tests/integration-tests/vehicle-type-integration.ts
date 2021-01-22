ac58f5e27f7e35538bc073e2491aea06e016aaf9
Nuno Dinis <1161042@isep.ipp.pt>
2021-01-22 23:13:47 +0000

    Vehicle type integration test.

--- RedeMasterData/src/tests/integration-tests/vehicle-type-integration.ts
+++ RedeMasterData/src/tests/integration-tests/vehicle-type-integration.ts
@@ -1,18 +1,67 @@
 import chai from 'chai';
-import { describe }  from 'mocha';
+import { describe } from 'mocha';
 import { app } from '../../app';
 import chaiHttp from 'chai-http';
 import 'mocha';
 import { types } from 'joi';
+import MongoClient from 'mongodb'
+import { mongo } from 'mongoose';
 process.env.APP_PORT = "3000"
 chai.use(chaiHttp)
 const expect = chai.expect;
+import {mongooseLoader} from '../../loaders/mongoose'
+
 let result: any;
-describe('Get /vehicle-types', function() {
-    it('verifies correct create', async function() {
-      result = await chai.request(app)
-      .get('/api/vehicle-types')
-   
-    expect(result).to.have.status(200);
-})
+describe('Get /vehicle-types', () => {
+    it('verfiies corret create', async () => {
+
+        result = await chai.request(app)
+            .post('/api/vehicle-types')
+            .send({
+                description: 'Tipo de veiculo',
+                autonomy: 50,
+                costPerKilometer: 50,
+                averageCost: 50,
+                averageSpeed: 50,
+                fuelType: 'Gasoleo'
+            })
+        expect(result).to.have.status(200);
+        expect(result.body).to.have.property('description', 'Tipo de veiculo')
+        expect(result.body).to.have.property('autonomy', 50)
+        expect(result.body).to.have.property('costPerKilometer', 50)
+        expect(result.body).to.have.property('averageCost', 50)
+        expect(result.body).to.have.property('averageSpeed', 50)
+        expect(result.body).to.have.property('fuelType', 'Gasoleo')
+    });
+    it('verfiies error create', async () => {
+
+        result = await chai.request(app)
+            .post('/api/vehicle-types')
+            .send({
+                description: 'Tipo de veiculo error',
+                autonomy: 50,
+                costPerKilometer: 50,
+                averageCost: 50,
+                averageSpeed: 50,
+                fuelType: 'Azeite'
+            })
+        expect(result).to.have.status(400);
+        expect(result.body[0]).to.equal('Fuel type Azeite is not allowed')
+    });
+    it('verfiies error create with nulls', async () => {
+
+        result = await chai.request(app)
+            .post('/api/vehicle-types')
+            .send({
+                description: 'Tipo de veiculo error',
+                autonomy: 50
+            })
+        expect(result).to.have.status(400);
+        expect(result.body.message).to.equal('some fields are missing or have invalid values')
+    });
+    it('verifies correct get', async function () {
+        result = await chai.request(app)
+            .get('/api/vehicle-types')
+        expect(result).to.have.status(200);
+    })
 })
