69c9a3b8c688daf165b62ddc80865008c27034ab
Nuno Dinis <1161042@isep.ipp.pt>
2021-01-23 00:18:15 +0000

    Tripulant Types tests

---
+++ RedeMasterData/src/tests/integration-tests/tripulant-type-integration.ts
@@ -0,0 +1,40 @@
+import chai from 'chai';
+import { describe } from 'mocha';
+import { app } from '../../app';
+import chaiHttp from 'chai-http';
+import 'mocha';
+import { types } from 'joi';
+import MongoClient from 'mongodb'
+import { mongo } from 'mongoose';
+process.env.APP_PORT = "3000"
+chai.use(chaiHttp)
+const expect = chai.expect;
+import {mongooseLoader} from '../../loaders/mongoose'
+
+let result: any;
+describe('Get /vehicle-types', () => {
+    it('verfiies corret create', async () => {
+
+        result = await chai.request(app)
+            .post('/api/tripulant-types')
+            .send({
+                description: 'Tipo de tripulante',
+            })
+        expect(result).to.have.status(200);
+        expect(result.body).to.have.property('description', 'Tipo de tripulante')
+    });
+    it('verfiies error create with nulls', async () => {
+
+        result = await chai.request(app)
+            .post('/api/tripulant-types')
+            .send({
+            })
+        expect(result).to.have.status(400);
+        expect(result.body.message).to.equal('some fields are missing or have invalid values')
+    });
+    it('verifies correct get', async function () {
+        result = await chai.request(app)
+            .get('/api/tripulant-types')
+        expect(result).to.have.status(200);
+    })
+})
