30e8491904f5d68a4ca6736cb81a6779d13e1746
Nuno Dinis <1161042@isep.ipp.pt>
2021-01-23 00:13:37 +0000

    Node integration tests

---
+++ RedeMasterData/src/tests/integration-tests/node-integration-tests.ts
@@ -0,0 +1,60 @@
+import chai from 'chai';
+import { describe } from 'mocha';
+import { app } from '../../app';
+import chaiHttp from 'chai-http';
+import 'mocha';
+import { types } from 'joi';
+import MongoClient from 'mongodb'
+import { mongo } from 'mongoose';
+process.env.APP_PORT = "3000"
+chai.use(chaiHttp)
+const expect = chai.expect;
+import {mongooseLoader} from '../../loaders/mongoose'
+
+let result: any;
+describe('Create /nodes', () => {
+    it('verfiies corret create', async () => {
+        result = await chai.request(app)
+            .post('/api/nodes')
+            .send({
+                shortName: 'GNDIntegration',
+                name: 'name',
+                longitude: '50',
+                latitude: '50',
+                collectionNode: false,
+                surrenderNode: false    
+            })
+        expect(result).to.have.status(200);
+        expect(result.body).to.have.property('shortName', 'GNDIntegration')
+        expect(result.body).to.have.property('name', 'name')
+        expect(result.body).to.have.property('longitude', 50)
+        expect(result.body).to.have.property('latitude', 50)
+        expect(result.body).to.have.property('collectionNode', false)
+        expect(result.body).to.have.property('surrenderNode', false)
+    });
+    it('verifies nulls in node', async () => {
+        result = await chai.request(app)
+            .post('/api/nodes')
+            .send({
+                shortName: 'GNDIntegration',
+            })
+        expect(result).to.have.status(400);
+
+        expect(result.body.message).to.equal('some fields are missing or have invalid values')
+    });
+    it('verifies validation failed', async () => {
+        result = await chai.request(app)
+            .post('/api/nodes')
+            .send({
+                shortName: 'Validation Failed',
+                name: 'name',
+                longitude: '50',
+                latitude: '50',
+                collectionNode: false,
+                surrenderNode: true    
+            })
+        expect(result).to.have.status(400);
+
+        expect(result.body[0]).to.equal('A Surrender Node must always be a Collection Node.')
+    });
+})
