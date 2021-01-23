58ee4c2225543df2c011a1a304338ba5d909d276
Jorge Gabriel Azevedo <1160929@isep.ipp.pt>
2021-01-23 03:42:16 +0000

    Add deployment test config

---
+++ Kubernets/scripts/test-deployment.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+sleep_time=5
+max_tries=5
+tries=0
+success=1
+
+while [ "$success" != 0 ] && [ "$tries" -lt "$max_tries" ]
+do
+
+    tries=$((tries+1))
+    echo "Starting try $tries"
+
+    kubectl_output=$(kubectl get pods)
+    runnings_pods=$(echo "$kubectl_output" | grep -c "Running")
+    output_lines=$[$(echo "$kubectl_output" | wc -l) - 1]
+
+    #success="$( [[ $runnings_pods == $output_lines ]]; echo $? )"
+    if [[ $runnings_pods == $output_lines ]]; then
+        success=0
+    elif [[ $runnings_pods != $output_lines ]] && [ "$tries" -lt "$max_tries" ]; then
+        success=1
+        sleep_time=$((sleep_time*tries))
+        echo "Sleep for $sleep $sleep_time s"
+        sleep $sleep_time
+    else 
+        success=1
+    fi
+
+done
+
+exit $success
