55bcb2538ef079ce7078e4605a68d7c1868becfc
Nuno Dinis <1161042@isep.ipp.pt>
2021-01-24 16:30:15 +0000

    Healthcheck

---
+++ ViagemMasterData/ViagemMasterData/Controllers/healthcheckController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ViagemMasterData.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class healthcheckController : Controller
+    {
+
+        [HttpGet]
+        public string GetHealthy()
+        {
+                return "I'm alive";  
+        }
+    }
+}
